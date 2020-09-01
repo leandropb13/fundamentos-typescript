f9e9bc83b943fd8e6f26e14762f6eb873ffa9dd9
Leandro Sidnei Atanazio <45641@PROWAY.TREINA>
2020-09-01 20:45:39 -0300

    exemplos da aula

---
+++ scripit.js
@@ -0,0 +1,20 @@
+var x = 5;
+var y = 9;
+
+var somar = (x + y);
+var subtrair = (x - y);
+var dividir = (x / y);
+var multiplicar = (x * y);
+/*
+alert("soma é: " + somar);
+alert("subtração é: " + subtrair);
+alert("divisão é: " + dividir);
+alert("multiplicação é: " + multiplicar);
+*/
+var ternario = somar >= 0 ? alert("é maior") : alert("é menor");
+
+function somando(a, b){
+    alert("O valor da soma é " + (a+b ));
+}
+
+somando(1,3);
