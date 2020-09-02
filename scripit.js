3d62eaace7a042c59dce2e3bb45d977cd78f5dcb
Leandro Sidnei Atanazio <45641@PROWAY.TREINA>
2020-09-01 22:07:23 -0300

    exemplos da aula de typescript 2

--- scripit.js
+++ scripit.js
@@ -18,3 +18,51 @@ function somando(a, b){
 }
 
 somando(1,3);
+
+for(var i=0; i< 10; i++ ){
+    console.log("O valor de i ->" + i);
+}
+
+// o number é um ponto flutuante,
+// hexa, octal e binario
+let meuOctal : number = 0o13;
+alert(meuOctal);
+
+let meuBinario: number = 0b11;
+alert(meuBinario);
+
+let meuHexa: number = 0xAB13;
+alert(meuHexa);
+
+let texto: string = "Olá Typescripit";
+alert(texto);
+
+let logradouro: string = "Rua 7 de Setembro";
+let numero: string = "123b";
+let cidade: string = "Blumenau";
+let estado: string = "SC";
+//alert("Endereço" + logradouro + ", " + numero + ". " + cidade + " - " + estado);
+
+// com interpolação de string
+alert(`Endereço: ${logradouro}, ${numero}. ${cidade} - ${estado} `);
+
+//tipo array em TS
+let meuArray: number [] = [1,2,3,4];
+alert(meuArray);
+
+//pode ser feito com o <> irá ter o mesmo efeito, e com isso fica tipado o array
+let meuArray2: Array<number> = [1,2,3,4];
+
+//operador com tuplas
+//exemplo de tuplas sao os dicionarios
+//palavra(string) -> significado(string)
+let campeaoBrasileiro: [string, number] = ["Palmeiras", 2016];
+let campeaoBrasileiro1: [string, number] = ["Corinthians", 2015];
+let campeaoBrasileiro2: [string, number] = ["Cruzeiro", 2014];
+
+alert(campeaoBrasileiro[0] + "-" + campeaoBrasileiro1[1]);
+
+let campeaoBrasileiro3: [string, number] = ["Palmeiras, 2016"];
+campeaoBrasileiro3[0] = "teste";
+
+alert(campeaoBrasileiro3);
